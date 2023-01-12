c4b3e930c430cb55428de001abe216c72f8b0199
Gil Portenseigne <pgil@apache.org>
2023-01-12 17:06:15 +0100

    Fix codenarc ConstantAssertExpression rules : Checks for assert statements where the assert boolean condition expression is a constant or literal value.

--- applications/party/src/main/groovy/org/apache/ofbiz/party/ContactMechWorkerTests.groovy
+++ applications/party/src/main/groovy/org/apache/ofbiz/party/ContactMechWorkerTests.groovy
@@ -132,7 +132,7 @@ class ContactMechWorkerTests extends OFBizTestCase {
                     foundOrderEmail = orderContactMechValueMap.contactMechPurposeType.contactMechPurposeTypeId == 'ORDER_EMAIL'
                     break
                 default:
-                    assert false
+                    fail()
                     break
             }
         }
@@ -171,7 +171,7 @@ class ContactMechWorkerTests extends OFBizTestCase {
                     foundPhone = true
                     break
                 default:
-                    assert false
+                    fail()
                     break
             }
         }
